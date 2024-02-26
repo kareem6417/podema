f45e62c713a2ef0da3574a40fb66c0aa9b291163
kareem6417 <132553583+kareem6417@users.noreply.github.com>
2024-02-26 16:50:37 +0700

    Update download.php

--- src/code-php/download.php
+++ src/code-php/download.php
@@ -146,7 +146,7 @@ $pdf->SetX(15);
 if ($totalScore > 100) {
     $recommendation = 'Berdasarkan pada hasil diatas, direkomendasikan untuk mengganti perangkat Anda dengan yang baru.';
 } else {
-    $recommendation = 'Berdasarkan pada hasil diatas, dinyatakan bahwa perangkat Anda masih dapat digunakan.' . "\n" .
+    $recommendation = 'Berdasarkan pada hasil diatas, dinyatakan bahwa perangkat Anda masih dapat digunakan.' .
                       'Oleh karena itu, tim IT akan melakukan peningkatan sesuai dengan kebutuhan perangkat Anda.';
 }
 
