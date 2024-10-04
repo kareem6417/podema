f39bc2f927a6bf69f2bdc62357b2a0c44d9de474
kareem6417 <132553583+kareem6417@users.noreply.github.com>
2024-10-04 09:35:32 +0700

    Update submit_inspeksi.php

--- src/code-php/submit_inspeksi.php
+++ src/code-php/submit_inspeksi.php
@@ -13,7 +13,6 @@ $conn = new mysqli($host, $user, $pass, $db);
             $age = isset($_POST["age"]) ? $_POST["age"] : '';
             $casing_lap = isset($_POST["casing_lap"]) ? $_POST["casing_lap"] : '';
             $layar_lap = isset($_POST["layar_lap"]) ? $_POST["layar_lap"] : '';
-            $keyboard_lap = isset($_POST["keyboard_lap"]) ? $_POST["keyboard_lap"] : '';
             $booting_lap = isset($_POST["booting_lap"]) ? $_POST["booting_lap"] : '';
             $multi_lap = isset($_POST["multi_lap"]) ? $_POST["multi_lap"] : '';
             $port_lap = isset($_POST["port_lap"]) ? $_POST["port_lap"] : '';
@@ -21,10 +20,10 @@ $conn = new mysqli($host, $user, $pass, $db);
             $software_lap = isset($_POST["software_lap"]) ? $_POST["software_lap"] : '';
     
             // Hitung skor
-            $score = $age + $casing_lap + $layar_lap + $keyboard_lap + $booting_lap + $multi_lap + $port_lap + $audio_lap + $software_lap;
+            $score = $age + $casing_lap + $layar_lap +  $booting_lap + $multi_lap + $port_lap + $audio_lap + $software_lap;
             
-            $sql = "INSERT INTO form_inspeksi (date, jenis, merk, lokasi, nama_user, status, serialnumber, informasi_keluhan, hasil_pemeriksaan, rekomendasi, age, casing_lap, layar_lap, keyboard_lap, booting_lap, multi_lap, port_lap, audio_lap, software_lap, score)
-                VALUES ('$date', '$jenis', '$merk', '$lokasi', '$nama_user', '$status', '$serialnumber', '$informasi_keluhan', '$hasil_pemeriksaan', '$rekomendasi', '$age', '$casing_lap', '$layar_lap', '$keyboard_lap', '$booting_lap', '$multi_lap', '$port_lap', '$audio_lap', '$software_lap', '$score')";
+            $sql = "INSERT INTO form_inspeksi (date, jenis, merk, lokasi, nama_user, status, serialnumber, informasi_keluhan, hasil_pemeriksaan, rekomendasi, age, casing_lap, layar_lap, booting_lap, multi_lap, port_lap, audio_lap, software_lap, score)
+                VALUES ('$date', '$jenis', '$merk', '$lokasi', '$nama_user', '$status', '$serialnumber', '$informasi_keluhan', '$hasil_pemeriksaan', '$rekomendasi', '$age', '$casing_lap', '$layar_lap', '$booting_lap', '$multi_lap', '$port_lap', '$audio_lap', '$software_lap', '$score')";
 
         } catch (\Throwable $th) {
         echo $th -> getMessage();
