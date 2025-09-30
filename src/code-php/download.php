5677096ac7a1e2619e87fc7dcf2cc93af9a29c61
kareem6417 <132553583+kareem6417@users.noreply.github.com>
2025-09-30 11:14:28 +0700

    Update download.php

--- src/code-php/download.php
+++ src/code-php/download.php
@@ -1,6 +1,9 @@
 <?php
 
-// Path ini SUDAH BENAR sesuai struktur folder Anda
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require('../fpdf/fpdf.php');
 
 $host = "mandiricoal.net";
