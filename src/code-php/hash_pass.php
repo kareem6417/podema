3ca588ce387da47cd173902575b4470dca4d1cc4
kareem6417 <132553583+kareem6417@users.noreply.github.com>
2025-10-16 09:52:18 +0700

    Update hash_pass.php

--- src/code-php/hash_pass.php
+++ src/code-php/hash_pass.php
@@ -1,12 +1,9 @@
 <?php
 
-// 1. Password Anda dimasukkan di sini
-$passwordAsli = 'senopati';
+$passwordAsli = 'sesayap';
 
-// 2. Proses ini mengubah password di atas menjadi kode hash yang aman
 $hashAman = password_hash($passwordAsli, PASSWORD_DEFAULT);
 
-// 3. Bagian ini menampilkan hasilnya di layar agar mudah disalin
 echo "<h1>Alat Pengaman Password</h1>";
 echo "<p>Gunakan kode di bawah ini untuk disimpan di database Anda.</p>";
 echo "<hr>";
